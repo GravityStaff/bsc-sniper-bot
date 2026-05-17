66e7fd577b4a2a1e263eb877be26fcc170c35431
BasinRainThaw <306958132+BasinRainThaw@users.noreply.github.com>
2026-05-18 01:43:53 +0300

    gas helpers (cont.)

--- sniper/utils.py
+++ sniper/utils.py
@@ -3,3 +3,6 @@ from web3 import Web3
 def to_wei(amount: float, unit: str = 'ether') -> int:
     return Web3.to_wei(amount, unit)
 
+def from_wei(amount: int, unit: str = 'ether') -> float:
+    return float(Web3.from_wei(amount, unit))
+
