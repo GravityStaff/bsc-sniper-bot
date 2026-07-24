2ede5e3ac91ddf329bbf330d7176c146ce3bf3af
BasinRainThaw <306958132+BasinRainThaw@users.noreply.github.com>
2026-07-25 02:17:56 +0300

    add gas buffer calc

--- sniper/utils.py
+++ sniper/utils.py
@@ -1,4 +1,5 @@
 from web3 import Web3
+import time
 
 def to_wei(amount: float, unit: str = 'ether') -> int:
     return Web3.to_wei(amount, unit)
@@ -6,6 +7,9 @@ def to_wei(amount: float, unit: str = 'ether') -> int:
 def from_wei(amount: int, unit: str = 'ether') -> float:
     return float(Web3.from_wei(amount, unit))
 
-def get_deadline(seconds: int = 120) -> int:
-    import time
+def get_deadline(seconds: int = 60) -> int:
     return int(time.time()) + seconds
+
+def apply_gas_multiplier(estimate: int, multiplier: float = 1.2) -> int:
+    """Don't want to get stuck in mempool just for a few gwei"""
+    return int(estimate * multiplier)
